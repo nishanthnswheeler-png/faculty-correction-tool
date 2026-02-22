b3ac19222645f266bee9e41e5fe9af3cbd81d7d8
KOLANUPAKA NAGARAJU <kolanupakanagaraju@KOLANUPAKAs-MacBook-Air.local>
2026-02-22 20:47:22 +0530

    fixed database init

--- app.py
+++ app.py
@@ -217,11 +217,16 @@ def attempt(test_id):
                            questions=questions)
 
 # ==========================
-# RUN (Railway Compatible)
+# CREATE TABLES (IMPORTANT)
+# ==========================
+
+with app.app_context():
+    db.create_all()
+
+# ==========================
+# RUN
 # ==========================
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    with app.app_context():
-        db.create_all()
     app.run(host="0.0.0.0", port=port)
