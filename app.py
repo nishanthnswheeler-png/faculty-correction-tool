57c99d496a9a63cef2de0bd543b1533aeb082ea6
KOLANUPAKA NAGARAJU <kolanupakanagaraju@KOLANUPAKAs-MacBook-Air.local>
2026-03-31 00:04:13 +0530

    fixed indentation error

--- app.py
+++ app.py
@@ -297,5 +297,5 @@ def manual_test(test_id):
 
 # ================= RUN =================
 if __name__ == "__main__":
-port = int(os.environ.get("PORT", 10000))
-app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
